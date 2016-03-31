944aec710820027d7252bedf2cf1728d545139c3
makudesu <makudesu@grex.org>
2016-03-31 14:56:58 +0800

    Added postgresql database

--- bnhs.py
+++ bnhs.py
@@ -17,7 +17,7 @@ from functools import wraps
 """config"""
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '128JSD*idfedf8ued89f7JHEDFjtw1143589123849iU*(UDF*D*F()D*F)(D*fjsdjfkj238490sdjfkjJDJFi(*)(&^&^*%tYYGHGhjBBb*H*hffJghgdfhkjk3eio2u3oiuqwoieuoiqyopolavofuiekghogsjdb*&&&DFOD&*F*(D&F*(DIOFUIKFHJDJHCKJVHJKCVkchvuhyiudyf8s9df98789743124789238UIOuFKAHDFKJAHDKLASHjkdgasgdhhasdgkjashdU(*&(*&*(*^^ASd876a7s6d87&&$^%$^#<F2>3234$#@121432!$25434%79^)*X&D(97_(A*Sd09POJZXd'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:root@localhost/thesis"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ljaavoeziccuxy:R1AYt8nzTJLR5tC-BfaM3fZ6sg@ec2-54-225-112-119.compute-1.amazonaws.com:5432/d9hrf2d6n9ifu9"
 """set the school year"""
 app.config['CSY'] = '2016-2017'
 """set the enrollment"""
@@ -350,5 +350,5 @@ admin.add_view(RegistrationView(Registration, db.session))
 if __name__ == '__main__':
 #    db.drop_all()
     db.create_all()
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False)
 
